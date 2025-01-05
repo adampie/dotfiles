553408f2a138f6d2eeb4aefceeaab31398d1df8f
Adam Pietrzycki <adam@pietrzycki.com>
2025-01-05 01:08:54 +0000

    trackpad stuff
    Signed-off-by: Adam Pietrzycki <adam@pietrzycki.com>

--- .chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
+++ .chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
@@ -2,10 +2,25 @@
 
 set -eufo pipefail
 
-trap 'killall "Dock"' EXIT
+trap 'killall "Dock" "Finder"' EXIT
 
 # Dock
 defaults write com.apple.dock "autohide" -bool "true" 
 defaults write com.apple.dock "autohide-time-modifier" -float "0.4"
 defaults write com.apple.dock "show-recents" -bool "false"
 defaults write com.apple.dock "tilesize" -int "30"
+
+# Finder
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
+defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf"
+defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true"
+defaults write NSGlobalDomain "NSDocumentSaveNewDocumentsToCloud" -bool "false"
+
+# Mission Control
+defaults write com.apple.dock "mru-spaces" -bool "false"
+
+# Trackpad
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad "Clicking" -bool "true"
+defaults write NSGlobalDomain "com.apple.mouse.tapBehavior" -int "1"
+defaults write NSGlobalDomain "com.apple.swipescrolldirection" -bool "false"
